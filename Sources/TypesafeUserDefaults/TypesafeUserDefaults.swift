162d378ea6d3863d75b7b6c09365a847dc518f0f
Andreas Kern <code@andreaskern.de>
2018-04-03 10:11:47 +0200

    Rename key types

--- Sources/TypesafeUserDefaults/TypesafeUserDefaults.swift
+++ Sources/TypesafeUserDefaults/TypesafeUserDefaults.swift
@@ -21,7 +21,7 @@ extension Int: DefaultsArchivable {}
 extension String: DefaultsArchivable {}
 extension URL: DefaultsArchivable {}
 
-public struct Key<T> where T: DefaultsArchivable {
+public struct DefaultsKey<T> where T: DefaultsArchivable {
     let name: String
 
     init(_ name: String) {
@@ -29,79 +29,79 @@ public struct Key<T> where T: DefaultsArchivable {
     }
 }
 
-public struct DefaultKey<T> where T: DefaultsArchivable {
+public struct DefaultsFallbackKey<T> where T: DefaultsArchivable {
     let name: String
-    let defaultValue: T
+    let fallbackValue: T
 
-    init(_ name: String, _ defaultValue: T) {
+    init(_ name: String, _ fallbackValue: T) {
         self.name = name
-        self.defaultValue = defaultValue
+        self.fallbackValue = fallbackValue
     }
 }
 
 extension UserDefaults {
-    public func value<T>(forKey key: Key<T>) -> T? {
+    public func value<T>(forKey key: DefaultsKey<T>) -> T? {
         return value(forKey: key.name) as? T
     }
 
-    public func value(forKey key: Key<URL>) -> URL? {
+    public func value(forKey key: DefaultsKey<URL>) -> URL? {
         return url(forKey: key.name)
     }
 
-    public func value<T: CustomDefaultsArchivable>(forKey key: Key<T>) -> T? {
+    public func value<T: CustomDefaultsArchivable>(forKey key: DefaultsKey<T>) -> T? {
         guard let jsonData = data(forKey: key.name) else {
             return nil
         }
         return try? JSONDecoder().decode(T.self, from: jsonData)
     }
 
-    public func value<T>(forKey key: DefaultKey<T>) -> T {
-        return value(forKey: key.name) as? T ?? key.defaultValue
+    public func value<T>(forKey key: DefaultsFallbackKey<T>) -> T {
+        return value(forKey: key.name) as? T ?? key.fallbackValue
     }
 
-    public func value(forKey key: DefaultKey<URL>) -> URL {
-        return url(forKey: key.name) ?? key.defaultValue
+    public func value(forKey key: DefaultsFallbackKey<URL>) -> URL {
+        return url(forKey: key.name) ?? key.fallbackValue
     }
 
-    public func value<T: CustomDefaultsArchivable>(forKey key: DefaultKey<T>) -> T {
+    public func value<T: CustomDefaultsArchivable>(forKey key: DefaultsFallbackKey<T>) -> T {
         guard let jsonData = data(forKey: key.name) else {
-            return key.defaultValue
+            return key.fallbackValue
         }
-        return (try? JSONDecoder().decode(T.self, from: jsonData)) ?? key.defaultValue
+        return (try? JSONDecoder().decode(T.self, from: jsonData)) ?? key.fallbackValue
     }
 
-    public func set<T>(_ value: T?, forKey key: Key<T>) {
+    public func set<T>(_ value: T?, forKey key: DefaultsKey<T>) {
         set(value, forKey: key.name)
     }
 
-    public func set(_ value: URL?, forKey key: Key<URL>) {
+    public func set(_ value: URL?, forKey key: DefaultsKey<URL>) {
         set(value, forKey: key.name)
     }
 
-    public func set<T: CustomDefaultsArchivable>(_ value: T?, forKey key: Key<T>) {
+    public func set<T: CustomDefaultsArchivable>(_ value: T?, forKey key: DefaultsKey<T>) {
         let jsonData = try? JSONEncoder().encode(value)
         set(jsonData, forKey: key.name)
     }
 
-    public func set<T>(_ value: T?, forKey key: DefaultKey<T>) {
+    public func set<T>(_ value: T?, forKey key: DefaultsFallbackKey<T>) {
         set(value, forKey: key.name)
     }
 
-    public func set(_ value: URL?, forKey key: DefaultKey<URL>) {
+    public func set(_ value: URL?, forKey key: DefaultsFallbackKey<URL>) {
         set(value, forKey: key.name)
     }
 
-    public func set<T: CustomDefaultsArchivable>(_ value: T?, forKey key: DefaultKey<T>) {
+    public func set<T: CustomDefaultsArchivable>(_ value: T?, forKey key: DefaultsFallbackKey<T>) {
         let jsonData = try? JSONEncoder().encode(value)
         set(jsonData, forKey: key.name)
     }
 
-    public subscript<T>(_ key: Key<T>) -> T? {
+    public subscript<T>(_ key: DefaultsKey<T>) -> T? {
         set { set(newValue, forKey: key) }
         get { return value(forKey: key) }
     }
 
-    public subscript<T>(_ key: DefaultKey<T>) -> T {
+    public subscript<T>(_ key: DefaultsFallbackKey<T>) -> T {
         set { set(newValue, forKey: key) }
         get { return value(forKey: key) }
     }
